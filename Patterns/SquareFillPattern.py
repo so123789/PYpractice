d17aae9edb2296967b6ea6bcbbbded6620e2975a
so123789 <souparnikac09@gmail.com>
2025-07-26 23:22:32 +0530

    patterns

---
+++ Patterns/SquareFillPattern.py
@@ -0,0 +1,15 @@
+
+# Square Fill Pattern
+# This code prints a square fill pattern of asterisks.
+# ***** 
+# ***** 
+# ***** 
+# ***** 
+# ***** 
+n = 5
+
+for i in range(0,n):
+    for j in range(0,n):
+        print("*",end="")
+    print(" ")
+    
